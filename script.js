ab4c5c5cb4cdfb70cf6ab23f9fba954361d93584
kostya <kostya1.6@yandex.ru>
2023-08-21 12:24:48 +0500

    finish0.9

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 // Начало Скрипта для вопрос - ответ
 const btnsDropdown = document.querySelectorAll('.answer');
-const test = document.querySelector('.answer__container');
 
-// const imgAnswer = btnsDropdown.querySelector('#answerPlus');
-// const titleAnswer = btnsDropdown.querySelector('#answerTitle');
-
-for (let index = 0; index < btnsDropdown.length; index++) {
-    const element = btnsDropdown[index];
-    element.id = index;
-    console.log(element);
-}
-
-btnsDropdown.addEventListener('click', function (e) {
-    console.log(e.target);
-    // e.target.classList.toggle('open');
-    // if (btnsDropdown.classList.value === "answer open") {
-    //     // imgAnswer.src = "/images/ico (1).svg";
-    //     // titleAnswer.style.color = "rgba(215, 72, 85, 1)";
-    // } else {
-    //     // imgAnswer.src = "/images/ico.svg";
-    //     // titleAnswer.style.color = "rgba(96, 56, 19, 1)";
-    // }
+// "/images/ico (2).svg"
+// e.target.offsetParent.children.id
+btnsDropdown.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        e.target.offsetParent.classList.toggle('open');
+        if (e.target.offsetParent.classList.value === 'answer open') {
+            console.log('open');
+            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico (1).svg";
+            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(215, 72, 85, 1)";
+        } else {
+            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico.svg";
+            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(96, 56, 19, 1)";
+        }
+        
+    })
 })
-
-
-
-// btnsDropdown.forEach(btn => {
-//     console.log(btn);
-//     btn.id = 1;
-// })
 
 // Конец Скрипта для вопрос - ответ
