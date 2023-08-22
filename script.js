2836159a82573b6bfac31c6bc1256ebcfebf2de1
kostya <kostya1.6@yandex.ru>
2023-08-22 17:52:52 +0500

    moreWork

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// // Начало Скрипта для вопрос - ответ
-// const btnsDropdown = document.querySelectorAll('.answer');
+// Начало Скрипта для вопрос - ответ
+const btnsDropdown = document.querySelectorAll('.answer');
 
-// btnsDropdown.forEach(btn => {
-//     btn.addEventListener('click', function (e) {
-//         e.target.offsetParent.classList.toggle('open');
-//         if (e.target.offsetParent.classList.value === 'answer open') {
-//             console.log('open');
-//             e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico (1).svg";
-//             e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(215, 72, 85, 1)";
-//         } else {
-//             e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico.svg";
-//             e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(96, 56, 19, 1)";
-//         }
+btnsDropdown.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        e.target.offsetParent.classList.toggle('open');
+        if (e.target.offsetParent.classList.value === 'answer open') {
+            console.log('open');
+            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico (1).svg";
+            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(215, 72, 85, 1)";
+        } else {
+            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico.svg";
+            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(96, 56, 19, 1)";
+        }
 
-//     })
-// })
-// // Конец Скрипта для вопрос - ответ
+    })
+})
+// Конец Скрипта для вопрос - ответ
 
 // // Начало Скрипта выдвижения окна слева
 // const headerSection = document.querySelector(".header");
@@ -35,15 +35,36 @@
 
 // Начало Скрипта сокращение колличства карточек врачей в зависимости от ширины экрана
 const expertsSection = document.querySelector('.experts');
-const expertCard = expertsSection.querySelectorAll('.expert__card');
+const expertCard = expertsSection.querySelectorAll('.expert__card'); 
+const expertProf = expertsSection.querySelectorAll('.experts__prof');
 
 window.onresize = resize;
+
+function hiddenTitle() {
+    expertProf.forEach(el => {
+        el.style.setProperty("visibility", "hidden");
+        el.style.setProperty("height", "0");
+    })
+}
+
+function visibileTitle() {
+    expertProf.forEach(el => {
+        el.style.setProperty("visibility", "visible");
+        el.style.setProperty("height", "auto");
+    })
+}
 
 function wdindowScreen () {
     for (let index = 0; index < expertCard.length; index++) {
         const element = expertCard[index];
         if (window.screen.width < 670 && index > 2) {
-            element.remove();
+            element.style.setProperty("visibility", "hidden");
+            element.style.setProperty("height", "0")
+            hiddenTitle()
+        } else {
+            element.style.setProperty("visibility", "visible");
+            element.style.setProperty("height", "auto")
+            visibileTitle()
         }
     }
 }
