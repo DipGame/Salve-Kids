af8d7ca5637b563e92208d98471eec33a02b9437
kostya <kostya1.6@yandex.ru>
2023-08-24 16:04:48 +0500

    startFix

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+// window.onresize = resize;
+
+// function resize() {
+
+// }
+
 // Начало Скрипта для вопрос - ответ
 const btnsDropdown = document.querySelectorAll('.answer');
 
 btnsDropdown.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        e.target.offsetParent.classList.toggle('open');
-        if (e.target.offsetParent.classList.value === 'answer open') {
-            console.log('open');
-            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico (1).svg";
-            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(215, 72, 85, 1)";
+    btn.addEventListener('click', function () {
+        btn.classList.toggle('open');
+        if (btn.classList.contains('open')) {
+            console.log('pop');
+            btn.querySelector('#answerPlus').src = "/images/ico (1).svg";
+            btn.querySelector('#answerTitle').style.color = "rgba(215, 72, 85, 1)";
         } else {
-            e.target.offsetParent.children[0].children.answerPlus.src = "/images/ico.svg";
-            e.target.offsetParent.children[0].children.answerTitle.style.color = "rgba(96, 56, 19, 1)";
+            btn.querySelector('#answerPlus').src = "/images/ico.svg";
+            btn.querySelector('#answerTitle').style.color = "rgba(96, 56, 19, 1)";
         }
-
     })
 })
 // Конец Скрипта для вопрос - ответ
@@ -21,17 +26,51 @@ btnsDropdown.forEach(btn => {
 const headerSection = document.querySelector(".header");
 const btnOpenWindow = headerSection.querySelector("#header-mobile__open-block");
 const containerWindow = headerSection.querySelector('.header-mobile__container');
-const btnCloseWindow = headerSection.querySelector('#header-mobile__close-block');
+const btnCloseWindow = headerSection.querySelector('.header-mobile__close-block');
 
 btnOpenWindow.addEventListener('click', function () {
-    containerWindow.style.setProperty("left", "0px");
-    btnCloseWindow.style.setProperty("left", "0px");
+    containerWindow.classList.add('header-mobile__container-open');
+    btnCloseWindow.classList.add('header-mobile__container-open');
 })
 btnCloseWindow.addEventListener('click', function () {
-    containerWindow.style.setProperty("left", "-390px");
-    btnCloseWindow.style.setProperty("left", "-390px");
+    containerWindow.classList.remove('header-mobile__container-open');
+    btnCloseWindow.classList.remove('header-mobile__container-open');
 })
 // Конец Скрипта выдвижения окна слева
+
+// Начало Скрипта слайдер отзывов при мобильной версии
+// const mainPage = document.querySelector('.main');
+// const reviewSection = mainPage.querySelector('.reviews');
+// const containerCardReview = reviewSection.querySelectorAll('.container__card-review');
+
+// containerCardReview.forEach(element => {
+//     console.log(element);
+// });
+// Конец Скрипта слайдер отзывов при мобильной версии
+
+// Начало Скрипта для спойлера
+const bottomTextbox = document.querySelectorAll('.bottom__text');
+
+bottomTextbox.forEach(el => {
+    const box = el.querySelector('.bottom__text-box')
+    const text = el.querySelector('#bottomText');
+    const btnMore = el.querySelector('.read__more');
+    console.log(btnMore.children[0]);
+    if (box.offsetHeight < text.offsetHeight) {
+        btnMore.classList.add('read__more_visual');
+    }
+    btnMore.addEventListener('click', () => {
+        box.classList.toggle('bottom__text-box-open');
+        if (box.classList.contains('bottom__text-box-open')) {
+            btnMore.children[0].textContent = "скрыть";
+            btnMore.children[1].classList.add('img__rotate');
+        } else {
+            btnMore.children[0].textContent = "читать далее";
+            btnMore.children[1].classList.remove('img__rotate');
+        }
+    })
+})
+// Конец Скрипта для спойлера
 
 // Начало Скрипта сокращение колличства карточек врачей в зависимости от ширины экрана
 // const expertsSection = document.querySelector('.experts');
