47bcacf7109d18248755043f0fb6930816cb059f
kostya <kostya1.6@yandex.ru>
2023-08-25 15:23:17 +0500

    src

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ btnsDropdown.forEach(btn => {
     btn.addEventListener('click', function () {
         btn.classList.toggle('open');
         if (btn.classList.contains('open')) {
-            console.log('pop');
             btn.querySelector('#answerPlus').src = "/images/ico (1).svg";
             btn.querySelector('#answerTitle').style.color = "rgba(215, 72, 85, 1)";
         } else {
@@ -24,19 +23,45 @@ btnsDropdown.forEach(btn => {
 
 // Начало Скрипта выдвижения окна слева
 const headerSection = document.querySelector(".header");
-const btnOpenWindow = headerSection.querySelector("#header-mobile__open-block");
+const btnOpenWindow = headerSection.querySelectorAll("#header-mobile__open-block");
 const containerWindow = headerSection.querySelector('.header-mobile__container');
 const btnCloseWindow = headerSection.querySelector('.header-mobile__close-block');
 
-btnOpenWindow.addEventListener('click', function () {
-    containerWindow.classList.add('header-mobile__container-open');
-    btnCloseWindow.classList.add('header-mobile__container-open');
+btnOpenWindow.forEach(btn => {
+    btn.addEventListener('click', function () {
+        containerWindow.classList.add('header-mobile__container-open');
+        btnCloseWindow.classList.add('header-mobile__container-open');
+    })
 })
 btnCloseWindow.addEventListener('click', function () {
     containerWindow.classList.remove('header-mobile__container-open');
     btnCloseWindow.classList.remove('header-mobile__container-open');
 })
 // Конец Скрипта выдвижения окна слева
+
+// Начало Скрипта выдвижение ховера услуг(Должен быть после Скрипта выдвижения окна слева!!!)
+const hoverBtnMenu = headerSection.querySelector('.hover__btn-menu');
+const moreMenu = headerSection.querySelector('.header-hover__list');
+const arrowMenu = headerSection.querySelector('.menu__add');
+hoverBtnMenu.addEventListener('click', () => {
+    moreMenu.classList.toggle('header-hover__list_open');
+    if (moreMenu.classList.contains('header-hover__list_open')) {
+        arrowMenu.classList.add('menu__add_rotate');
+    } else {
+        arrowMenu.classList.remove('menu__add_rotate');
+    }
+})
+// Конец Скрипта выдвижение ховера услуг
+
+// Начало Скрипта кнопки для прокрутки наверх страницы(Должен быть после Скрипта выдвижения окна слева!!!)
+headerSection.querySelector('.header-mobile__btn-up').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+})
+// Конец Скрипта кнопки для прокрутки наверх страницы
 
 // Начало Скрипта слайдер отзывов при мобильной версии
 // const mainPage = document.querySelector('.main');
@@ -55,7 +80,6 @@ bottomTextbox.forEach(el => {
     const box = el.querySelector('.bottom__text-box')
     const text = el.querySelector('#bottomText');
     const btnMore = el.querySelector('.read__more');
-    console.log(btnMore.children[0]);
     if (box.offsetHeight < text.offsetHeight) {
         btnMore.classList.add('read__more_visual');
     }
